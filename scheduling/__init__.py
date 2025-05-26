9b27a20d89bf35cdc456b53eba309c00221df0e7
pingmyheart <antoniorussi1972@gmail.com>
2025-05-26 19:10:44 +0200

    [ADD] - scheduler

--- scheduling/__init__.py
+++ scheduling/__init__.py
@@ -5,13 +5,22 @@ from apscheduler.triggers.cron import CronTrigger
 
 from configuration import bot
 from scheduling.ring_notification_scheduler import RingNotificationScheduler
+from scheduling.ring_reminder_notification_scheduler import RingReminderNotificationScheduler
 from service import user_service_bean, ring_service_bean
+from util import constant_bean
 
 ring_notification_scheduler_bean = RingNotificationScheduler(user_service_bean=user_service_bean,
                                                              ring_service_bean=ring_service_bean,
+                                                             constant_bean=constant_bean,
                                                              tg_bot=bot)
+
+ring_reminder_notification_scheduler_bean = RingReminderNotificationScheduler(user_service_bean=user_service_bean,
+                                                                              ring_service_bean=ring_service_bean,
+                                                                              constant_bean=constant_bean,
+                                                                              tg_bot=bot)
 
 scheduler = BackgroundScheduler(timezone=ZoneInfo("UTC"))
 cron_expr = CronTrigger.from_crontab("* * * * *")
 scheduler.add_job(ring_notification_scheduler_bean.schedule, cron_expr)
+scheduler.add_job(ring_reminder_notification_scheduler_bean.schedule, cron_expr)
 scheduler.start()
